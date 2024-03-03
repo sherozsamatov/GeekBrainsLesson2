3a87f5896650cc3a697c2b7bf63879bb44df83bc
Sheroz Samatov <sherozonline@gmail.com>
2024-03-03 15:42:48 +0000

    add task1

---
+++ Task 1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+int number = 322;
+
+if (number % 161 == 0) {
+  Console.WriteLine("The number is divisible by 7 and 23 at the same time");
+}
+
+else {
+  Console.WriteLine("The number is not divisible by 23 or 7");
+}
